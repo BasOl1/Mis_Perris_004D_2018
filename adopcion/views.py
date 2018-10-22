0bd64f35f52bfe2e9a3ef9df1759d6a74f541542
Bastian <bas.olguin@alumnos.duoc.cl>
2018-10-22 06:51:40 -0300

    Agregar templates a directorio adopcion. Se agregan los archivos html a adopcion/templates/adopcion

--- adopcion/views.py
+++ adopcion/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Interesado, Rescatado
 # Create your views here.
+def index(request):
+    return render(request, 'adopcion/index.html', {})
+
+def gallery(request):
+    return render(request, 'adopcion/gallery.html', {})
